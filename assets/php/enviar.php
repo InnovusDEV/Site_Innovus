869e4cf8373bfe26e1dea931bd35dc18b10dcb66
N4Y3L1 <58003310+N4Y3L1@users.noreply.github.com>
2022-03-12 10:15:09 -0600

    inicio

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -7,5 +7,5 @@
     $contenido = "Nombre" . $nombre . "\nCorreo" . $correo . "\nMensaje" . $mensaje;
     mail($destino, "Contacto de Innovus", $contenido);
     echo "<script>alert('Mensaje Enviado exitosamente')</script>";
-    echo "<script> setTimeout(\"location.href='https://nayeliaceves.000webhostapp.com/ethereal_Inovus/index.html'\",1000) </script>";
+    echo "<script> setTimeout(\"location.href='https://innovusweb.com/'\",1000) </script>";
 ?>
